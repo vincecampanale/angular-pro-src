06b4dfc0155a6896f58cccc97f5d2df34082ce81
Vince Campanale <vcampanale0294@gmail.com>
2017-05-02 08:30:16 -0400

    starting native element

--- 03-projecting-components/app/auth-form/auth-form.component.ts
+++ 03-projecting-components/app/auth-form/auth-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, Output, ViewChild, AfterViewInit, EventEmitter, ContentChildren, QueryList, AfterContentInit } from '@angular/core';
+import { Component, Output, ElementRef, ChangeDetectorRef, ViewChild, ViewChildren, AfterViewInit, EventEmitter, ContentChildren, QueryList, AfterContentInit } from '@angular/core';
+
+
 
 import { AuthRememberComponent } from './auth-remember.component';
 import { AuthMessageComponent } from './auth-message.component';
@@ -13,7 +15,7 @@ import { User } from './auth-form.interface';
         <ng-content select="h3"></ng-content>
         <label>
           Email address
-          <input type="email" name="email" ngModel>
+          <input type="email" name="email" ngModel #email>
         </label>
         <label>
           Password
@@ -27,30 +29,47 @@ import { User } from './auth-form.interface';
   `
 })
 export class AuthFormComponent implements AfterContentInit {
-
+  // Local Properties
   showMessage: boolean;
 
-  @ViewChild(AuthMessageComponent) message: AuthMessageComponent
+  @ViewChild('email') email: ElementRef;
+
+  // Note: ViewChildren is only available inside of ngAfterViewInit()
+  @ViewChildren(AuthMessageComponent) message: QueryList<AuthMessageComponent>;
 
   @ContentChildren(AuthRememberComponent) remember: QueryList<AuthRememberComponent>;
 
   @Output() submitted: EventEmitter<User> = new EventEmitter<User>();
 
+  // Constructor (Dependency Injections)
+  constructor(private cd: ChangeDetectorRef) { }
+
+
+  // Lifecycle hooks
+
   // if you want to change data before view has been initialized, make sure you use ngAfterContentInit() instead of ngAfterViewInit()
   ngAfterViewInit() {
-    // console.log(this.message);
+    console.log(this.email);
+    if (this.message) {
+      // setTimeout(() => { // hacky solution for changing the view after it's been rendered -- not required in production
+        this.message.forEach((message) => {
+          message.days = 30;
+        });
+      // });
+      this.cd.detectChanges();
+    }
   }
 
   ngAfterContentInit() {
-    if (this.message) {
-      this.message.days = 30;
-    }
     if (this.remember) {
       this.remember.forEach((item) => {
         item.checked.subscribe((checked: boolean) => this.showMessage = checked);
       });
     }
   }
+
+
+  // Methods 
 
   onSubmit(value: User) {
     this.submitted.emit(value);
