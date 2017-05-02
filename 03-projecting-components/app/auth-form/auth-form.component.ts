6dcd22d5251b932c36f95318680c7e52f3fcec3b
Vince Campanale <vcampanale0294@gmail.com>
2017-05-02 08:14:02 -0400

    starting view children

--- 03-projecting-components/app/auth-form/auth-form.component.ts
+++ 03-projecting-components/app/auth-form/auth-form.component.ts
@@ -1,6 +1,7 @@
-import { Component, Output, EventEmitter, ContentChildren, QueryList, AfterContentInit } from '@angular/core';
+import { Component, Output, ViewChild, AfterViewInit, EventEmitter, ContentChildren, QueryList, AfterContentInit } from '@angular/core';
 
 import { AuthRememberComponent } from './auth-remember.component';
+import { AuthMessageComponent } from './auth-message.component';
 
 import { User } from './auth-form.interface';
 
@@ -19,9 +20,7 @@ import { User } from './auth-form.interface';
           <input type="password" name="password" ngModel>
         </label>
         <ng-content select="auth-remember"></ng-content>
-        <div *ngIf="showMessage">
-          You will be logged in for 30 days.
-        </div>
+        <auth-message [style.display]="(showMessage ? 'inherit' : 'none')"></auth-message>
         <ng-content select="button"></ng-content>
       </form>
     </div>
@@ -31,11 +30,21 @@ export class AuthFormComponent implements AfterContentInit {
 
   showMessage: boolean;
 
+  @ViewChild(AuthMessageComponent) message: AuthMessageComponent
+
   @ContentChildren(AuthRememberComponent) remember: QueryList<AuthRememberComponent>;
 
   @Output() submitted: EventEmitter<User> = new EventEmitter<User>();
 
+  // if you want to change data before view has been initialized, make sure you use ngAfterContentInit() instead of ngAfterViewInit()
+  ngAfterViewInit() {
+    // console.log(this.message);
+  }
+
   ngAfterContentInit() {
+    if (this.message) {
+      this.message.days = 30;
+    }
     if (this.remember) {
       this.remember.forEach((item) => {
         item.checked.subscribe((checked: boolean) => this.showMessage = checked);
--- 03-projecting-components/app/auth-form/auth-message.component.ts
+++ 03-projecting-components/app/auth-form/auth-message.component.ts
@@ -9,6 +9,6 @@ import { Component } from '@angular/core';
     `
 })
 
-export class NameComponent {
+export class AuthMessageComponent {
     days: number = 7;
 }
